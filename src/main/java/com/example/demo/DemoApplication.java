902b28f93b54f739fab39cba480e6c043d604d0e
zhouguanglong1 <970711665@qq.com>
2019-03-15 15:06:31 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,5 +9,12 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	
+	@RequestMapping(value = "/test" ,method = RequestMethod.POST)
+	@ResponseBody
+    	public Map<String,Object> todayInvokingStat(@RequestBody Map<String,Object> req) throws Exception{
+        	Map<String,Object> result = new HashMap<>();
+		result.put("name","hello,I'm v1!");
+        return result;
+    }
 }
